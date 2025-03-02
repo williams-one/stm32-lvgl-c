1b7d0f51bfa01695704276bb79f186c1adf3fbc5
William Spinelli <174336620+williams-one@users.noreply.github.com>
2025-03-02 19:06:54 +0100

    Add touch screen support

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -143,7 +143,7 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
   	lv_timer_handler();
-  	// HAL_Delay(1);
+  	HAL_Delay(1);
 
     update_image_position();
   }
--- hal_stm_lvgl/touchpad/touchpad.c
+++ hal_stm_lvgl/touchpad/touchpad.c
@@ -10,6 +10,7 @@
 #include "lvgl/lvgl.h"
 
 #include "stm32u5xx.h"
+#include "main.h"
 
 #include <stdio.h>
 
@@ -39,8 +40,17 @@ extern I2C_HandleTypeDef hi2c1;
  *   GLOBAL FUNCTIONS
  **********************/
 
+static void reset_touchpad(void)
+{
+  HAL_GPIO_WritePin(TS_RST_GPIO_Port, TS_RST_Pin, GPIO_PIN_RESET);
+  HAL_Delay(10);
+  HAL_GPIO_WritePin(TS_RST_GPIO_Port, TS_RST_Pin, GPIO_PIN_SET);
+}
+
 void touchpad_init(void)
 {
+  reset_touchpad();
+
   lv_indev_t* indev = lv_indev_create();
   lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
   lv_indev_set_read_cb(indev, touchpad_read);
@@ -52,36 +62,33 @@ void touchpad_init(void)
 
 static void touchpad_read(lv_indev_t* indev, lv_indev_data_t* data)
 {
-//  uint8_t touches = 0;
-//  uint8_t buf[6];
-//  const uint16_t STATUS_REG = 0x814E;
-//  const uint16_t TOUCH_POS_REG = 0x8150;
-//  uint8_t ZERO = 0;
-//
-//  static int16_t last_x = 0;
-//  static int16_t last_y = 0;
-//
-//  HAL_I2C_Mem_Read(&hi2c1, 0xBA, STATUS_REG, 2, buf, 1, HAL_MAX_DELAY);
-//  touches = (0x0F & buf[0]);
-//
-//  HAL_I2C_Mem_Write(&hi2c1, 0xBA, STATUS_REG, 2, &ZERO, 1, HAL_MAX_DELAY);
-//
-//  if (touches > 0)
-//  {
-//    HAL_I2C_Mem_Read(&hi2c1, 0xBA, TOUCH_POS_REG, 2, buf, 4, HAL_MAX_DELAY);
-//    int16_t x = buf[0] + (buf[1] << 8);
-//    int16_t y = buf[2] + (buf[3] << 8);
-//
-//    data->point.x = x;
-//    data->point.y = y;
-//    last_x = data->point.x;
-//    last_y = data->point.y;
-//    data->state = LV_INDEV_STATE_PRESSED;
-//  }
-//  else
-//  {
-//    data->point.x = last_x;
-//    data->point.y = last_y;
-//    data->state = LV_INDEV_STATE_RELEASED;
-//  }
+  uint8_t buf[5] = {0};
+  const uint8_t FT5446_I2C_ADDR = 0x38 << 1;
+  const uint8_t FT5446_TOUCH_REG = 0x02;
+
+  static int16_t last_x = 0;
+  static int16_t last_y = 0;
+
+  if (HAL_I2C_Mem_Read(&hi2c1, FT5446_I2C_ADDR, FT5446_TOUCH_REG, I2C_MEMADD_SIZE_8BIT, buf, sizeof(buf), HAL_MAX_DELAY) == HAL_OK)
+  {
+    uint8_t touches = buf[0] & 0x0F;
+
+    if (touches > 0)
+    {
+      int16_t x = ((buf[1] & 0x0F) << 8) | buf[2];
+      int16_t y = ((buf[3] & 0x0F) << 8) | buf[4];
+
+      data->point.x = x;
+      data->point.y = y;
+      last_x = data->point.x;
+      last_y = data->point.y;
+      data->state = LV_INDEV_STATE_PRESSED;
+    }
+    else
+    {
+      data->point.x = last_x;
+      data->point.y = last_y;
+      data->state = LV_INDEV_STATE_RELEASED;
+    }
+  }
 }
